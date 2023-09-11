8347349924f32d871cbc9b75481f48561cc4c3a0
Warlie Yap <137013560+warlie3d@users.noreply.github.com>
2023-09-11 17:56:56 -0400

    finished homework

--- factory.js
+++ factory.js
@@ -1,3 +1,21 @@
 class Factory{
-    
+    constructor(location, owner) {
+        this.location = location
+        this.owner = owner
+        this.vehicles = []     
+    }
+    listVehicles() {
+        return this.vehicles
+    }
+    createVehicle(vehicle) {
+        this.vehicles.push(vehicle);
+        //console.log(this.vehicles);
+    }
+    paintVehicle(vin, paintColor) {
+        this.vehicles.find(function (v) {
+            if (v.vin === vin) {
+                v.paint(paintColor)
+            }
+        })
+    }
 }
--- vehicle.js
+++ vehicle.js
@@ -1,4 +1,27 @@
 class Vehicle {
-    
+    constructor(make, model, year, price, vin, color) {
+        this.make = make
+        this.model = model
+        this.year = year
+        this.color = color
+        this.price= price
+        this.vin = vin     
+    }
+    drive() {
+        return (this.year + " " + this.make + " " + this.model + " drives");
+    }
+    paint(color) {
+        return this.color = color     
+    }    
 }
 
+// notes                                
+//     drive() {
+//         return this.model + " " + "drives"
+//     }
+// }
+// class Truck extends Vehicle {
+//     constructor(model, towingCapacity) {
+//         super(model)
+//         this.towingCapacity = towingCapacity;      
+//     }
